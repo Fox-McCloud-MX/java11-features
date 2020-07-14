3726f5a8d122142c4b815ae3d410c6166c71eba7
framirez90 <kdsfoxy@gmail.com>
2020-07-14 11:14:13 -0500

    Repeat

---
+++ src/main/java/com/mx/java11/features/_string/Repeat.java
@@ -0,0 +1,8 @@
+package com.mx.java11.features._string;
+
+public class Repeat {
+    public static void main(String[] args) {
+        String str = "=".repeat(5);
+        System.out.println(str); //prints ======
+    }
+}
